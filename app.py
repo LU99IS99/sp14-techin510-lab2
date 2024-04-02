40492c21c8f2d2ab118516c2069a2d4d76ff345a
Junyan Lu <junyanlu@Junyans-MacBook-Pro.local>
2024-04-02 15:52:21 -0700

    updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+
 st.set_page_config(
     page_title="Penguins Explorer",
     page_icon="🐧",
@@ -9,4 +10,20 @@ st.set_page_config(
 )
 df = pd.read_csv('https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv')
 
+
+bill_length_slider = st.slider(
+    "Bill Length(mm)",
+    min(df["bill_length_mm"]),
+    max(df["bill_length_mm"])
+)
+
+df = df[df["bill_length_mm"] > bill_length_slider]
+
+st.selectbox("Species", df["Adelle", "Chinstrap"])
+
+st.selectbox("Species", df["species".unique()])
+
+st.multiselect("Island", df["island"].unique())
+
 st.write(df.head())
+

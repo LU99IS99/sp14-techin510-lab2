a0f012bbaed3f57a0841b29c884da94b32878ba5
Junyan Lu <junyanlu@Junyans-MacBook-Pro.local>
2024-04-02 14:56:45 -0700

    updated

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import seaborn as sns
+
+st.set_page_config(
+    page_title="Penguins Explorer",
+    page_icon="🥺",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None
+)
+df = pd.read_csv('https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv')
+
+st.write(df.head())
